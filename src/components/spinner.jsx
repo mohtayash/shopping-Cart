628f174d6cabe82c173509dd611b1f2df5b4be9e
yash_mohta <89544906+mohtayash@users.noreply.github.com>
2023-07-15 23:44:05 +0530

    Add files via upload

---
+++ src/components/spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./spinner.css"
+
+const Spinner = () => {
+  return (
+    <div>
+      <div className='spinner'></div>
+    </div>
+  )
+}
+
+export default Spinner
